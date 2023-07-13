29da97564f6833b76f8b8c65d8e53afcc0a7ed30
HANSEOL <nothinonthere@icoud.com>
2023-07-13 17:22:48 +0900

    [update] 오후 작업 update(FileINOUT)

--- src/FileInOut.java
+++ src/FileInOut.java
@@ -1,3 +1,48 @@
+import java.io.*;
+
 public class FileInOut {
+    void addbook(Book newone) {
+        try {
+            OutputStream dir = new FileOutputStream("/Users/hanseol/HelloWorld/data.txt");
+            byte[] by = newone.getBookname().getBytes();
+            byte[] sharp = "#".getBytes();
+            dir.write(by);
+            dir.write(sharp);
+            by = newone.getWriter().getBytes();
+            dir.write(by);
+            dir.write(sharp);
+            by = newone.getCompany().getBytes();
+            dir.write(by);
+            dir.write(sharp);
+            by = new byte[]{(byte) (newone.getdiditRent() ? 1 : 0)};
+            dir.write(by);
+            dir.write(sharp);
+            by = newone.getRentname().getBytes();
+            dir.write(by);
+            dir.write(sharp);
+            by = newone.getRentday().getBytes();
+            dir.write(by);
+            dir.write(sharp);
+            by = newone.getReturnday().getBytes();
+            dir.write(by);
+            sharp = "\n".getBytes();
+            dir.write(sharp);
+
+        }
+        catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+
+    void fileRead() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("/Users/hanseol/HelloWorld/data.txt"));
+        while(true) {
+            String line = br.readLine();
+            if(line == null) break;
+            System.out.println(line);
+        }
+        br.close();
+    }
+
 
 }
